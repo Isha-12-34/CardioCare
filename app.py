713da5650112a3e4fca7a559c84ecb9664b4789d
Isha-12-34 <ishashingote@gmail.com>
2026-03-07 21:03:11 +0530

    Change home route to '/login.html'

--- app.py
+++ app.py
@@ -73,9 +73,8 @@ CardioCare Team
 create_tables()
 
 # ---------------- HOME ----------------
-@app.route('/')
-def home():
-    return render_template('login.html')
+@app.route('/login.html')
+
 
 
 # ---------------- REGISTER ----------------
